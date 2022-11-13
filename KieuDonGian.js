67098a7daaaba5088c84da320df390af32d687c8
Hàn Quá Nhanh <hanquanhanh@gmail.com>
2022-11-13 22:08:28 +0700

    Bai 3

---
+++ KieuDonGian.js
@@ -0,0 +1,4 @@
+//let ten: string = "Qua Nhanh";
+//let ten = "Qua Nhanh";
+var laTen = true;
+console.log(typeof laTen);
---
+++ KieuDonGian.ts
@@ -0,0 +1,6 @@
+//let ten: string = "Qua Nhanh";
+//let ten = "Qua Nhanh"1;
+
+let laTen = true;
+
+console.log(typeof laTen);
